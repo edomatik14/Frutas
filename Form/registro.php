a3ab6bc9105b9aafc59a95fa83b002749b66d4e8
Edomatik14 <deaos1497@gmail.com>
2023-01-24 21:41:22 -0500

    first commit

---
+++ Form/registro.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro</title>
+</head>
+<body>
+    <?php
+    $servername = "localhost";
+    $database = "taller";
+    $username = "root";
+    $password = "";
+
+    $nombre = $_POST['nombre'];
+    $tipo = $_POST['tipo'];
+    $color = $_POST['color'];
+    
+    $conn = mysqli_connect($servername,$username,$password,$database);
+    
+    $sql = "INSERT INTO frutas(nombre,tipo,color)
+    VALUES ('$nombre','$tipo','$color')";
+
+    if(mysqli_query($conn,$sql)){
+        echo "Insercion de datos exitosa :3";
+    }else{
+        echo "Fallo la insercion de datos";
+    }
+
+    mysqli_close($conn);
+    ?>
+    <br>
+    <a href="index.html">Volver</a>
+    <br>
+    <a href="../index.html" >INICIO</a>
+</body>
+</html>
---
+++ Tabla/index.php
@@ -0,0 +1,66 @@
+<?php
+$servername = "localhost";
+$database = "taller";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Consulta frutas</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+    <header>
+        <div class="cointainer-fluid">
+            <div class="row justify-content-center mt-2s">
+                <div class="col-auto pt-2">
+                    <h4>TABLA DE FRUTAS</h4>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <table class="table table-hover table-responsive">
+                        <thead class="thead-default thead-inverse">
+                            <th>NOMBRE</th>
+                            <th>TIPO</th>
+                            <th>COLOR</th>
+                        </thead>
+                        <?php
+                        $sql="SELECT * FROM frutas";
+                        $result = mysqli_query($conn,$sql);
+                        
+                        if (mysqli_num_rows($result) > 0){
+                            while ($row = mysqli_fetch_assoc($result)){
+                                	echo "<tr>
+                                            <td>".$row["nombre"]."</td>
+                                            <td>".$row["tipo"]."</td>
+                                            <td>".$row["color"]."</td>
+                                          </tr>";
+                            }
+                        }else{
+                            echo "No hubo resultados";
+                        }
+                        mysqli_close($conn);
+                        ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
